3c077f4bc58e116dc9197c550fa40d81ae4d88c1
Sharon Rumsey <shazzadevacademy@gmail.com>
2015-12-20 08:50:39 +1300

    add script file

---
+++ script.js
@@ -0,0 +1,19 @@
+function checkScriptLoad() {
+  chrome.runtime.onMessage.addListener(function(request, sender, response) {
+    if (request.msg == 'is_page_capturable') {
+      try {
+        if (isPageCapturable()) {
+          response({msg: 'capturable'});
+        } else {
+          response({msg: 'uncapturable'});
+        }
+        return true;
+      } catch(e) {
+        response({msg: 'loading'});
+        return true;
+      }
+    }
+    return true;
+  });
+}
+checkScriptLoad();
