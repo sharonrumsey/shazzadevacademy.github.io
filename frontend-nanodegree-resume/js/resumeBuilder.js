ba73805c08f03e0ff551f0d731d099668a2ac399
Sharon Rumsey <shazzadevacademy@gmail.com>
2016-01-03 23:12:09 +1300

    start on Udacity resume

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -1,3 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// var awesomeThoughts = "I am Sharon and I am AWESOME!";
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("AWESOME" , "FUN");
+// $("#main").append(funThoughts);
+
+var name = "Sharon Rumsey";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%" , name);
+var formattedRole = HTMLheaderRole.replace("%data%" , role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
