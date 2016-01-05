933404a6afc57c9aebb869bb1f9cc060d8172f49
Sharon Rumsey <shazzadevacademy@gmail.com>
2016-01-05 21:16:36 +1300

    add bio to resume

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -15,3 +15,22 @@ var formattedRole = HTMLheaderRole.replace("%data%" , role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var skills = ["awesomeness" , " programming ", "JS "];
+// $("#main").append(skills);
+//$("#main").append(skills[0]);
+
+var bio = {
+  "name" : name,
+  "role" : role,
+  "contact_info" : "sharon@rumsey.nz",
+  "picture" : '<img src="../frontend-nanodegree-resume/images/sharon.jpg">',
+  "welcome_message" : "Hire Me!",
+  "skills" : skills,
+};
+$("#main").append(bio.name + " ")
+          .append(bio.role + " ")
+          .append(bio.contact_info + " ")
+          .append(bio.picture)
+          .append(bio.welcome_message + " ")
+          .append(bio.skills);
