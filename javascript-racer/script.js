b561f902cc24cd7cafea403dc340a648052e3e07
Sharon Rumsey <shazzadevacademy@gmail.com>
2016-01-15 13:07:03 +1300

    complete racer

--- javascript-racer/script.js
+++ javascript-racer/script.js
@@ -1,25 +1,42 @@
-//Game Play - racing click of keys q 113 and p 112
-function keyhandler(event) {
+// variables to identify racers and message
+var racer;
+var msg;
+
+//init function invoked when page is loaded
+//id for the racer, pos for the position and msg to id winner
+function init() {
+  document.onkeypress=keyPressHandler;
+  racer = [ { "id" : document.getElementById("racer0"),
+              "pos" : 0,
+              "msg" : "Red Racer Wins!"},
+            { "id" : document.getElementById("racer1"),
+              "pos" : 0,
+              "msg" : "Green Racer Wins!"},
+          ];
+    msg = document.getElementById( "msg" );
+}
+//key press event for moving racer along track
+function keyPressHandler(event) {
   var e = event || window.event;
-  switch( e.keyCode ) {
-    case 112: advancePlayer(1);  break;
-    case 113: advancePlayer(2);  break;
+  switch(e.charCode) {
+    case 113: moveRacer(0); break; //Q
+    case 112: moveRacer(1); break; //P
   }
 }
 
-//Moving the player on the table - inserting space behind keystroke
-function advancePlayer(player) {
-
+//Moving the player/Racer - inserting spaces behind racer with keystrokes
+function moveRacer(n) {
+  if (racer[n].pos < 96 ) {
+    racer[n].pos += 4;
+    racer[n].id.style.marginLeft = racer[n].pos.toString() + "%";
+  }
+  if (racer[n].pos == 96 && racer[1-n].pos < 96) {
+    msg.innerHTML = racer[n].msg;
+  }
+  //to test
+  console.log("moveRacer " + n.toString() + " " + racer[n].pos.toString() + "%");
 }
 
-//Build race track
-var trackLength = 20;
-for ( i = 1; i < trackLength; i++ ) {
-  document.getElementById("player1_strip").append(document.createElement( "td"));
-  document.getElementById("player2_strip").append(document.createElement( "td"));
-};
-
-//Reset Track
 
 
-//Notify who wins with the words You Win - BARKERS!
+
